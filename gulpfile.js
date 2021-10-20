d8720d7c4ece9a37bc56412efe845c0faef248e8
Peter Abah <peterabah.ob@gmail.com>
2021-10-20 12:22:35 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const { src, dest, watch, series } = require('gulp');
+const postcss = require('gulp-postcss');
+
+const css = (callback) => {
+  const processors = [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require('cssnano')
+  ];
+
+  src('./src/css/**/*.css')
+    .pipe(postcss(processors))
+    .pipe(dest('./css'));
+
+  callback();
+};
+
+const watchFiles = (callback) => {
+  watch('./index.html', css);
+  callback();
+}
+
+exports.default = series(css, watchFiles);
+exports.css = css;
---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    contents: ['/index.html']
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'sans': ['Poppins', 'Roboto', 'sans-serif'],
+      'display': ['Heebo', 'Georgia', 'serif'],
+    },
+
+    colors: {
+      white: 'fff'
+      gray: {
+        1: 'var(--gray1)',
+        2: 'var(--gray2)',
+        3: 'var(--gray3)',
+        4: 'var(--gray4)',
+        5: 'var(--gray5)',
+        6: 'var(--gray6',
+      },
+
+      blue: 'var(--blue)',
+      darkblue: 'var(--dark-blue)',
+      red: 'var(--red)',
+      green: 'var(--green)',
+    },
+
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
