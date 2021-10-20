f72cfa38ee76d7986a57f098245b38e07d1ebfed
Peter Abah <peterabah.ob@gmail.com>
2021-10-20 22:16:46 +0100

    add styles for mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: {
     enabled: true,
-    contents: ['/index.html']
+    content: ['index.html'],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
@@ -11,23 +11,40 @@ module.exports = {
     },
 
     colors: {
-      white: 'fff',
+      transparent: 'transparent',
+      white: '#fff',
       gray: {
         1: 'var(--gray1)',
         2: 'var(--gray2)',
         3: 'var(--gray3)',
         4: 'var(--gray4)',
         5: 'var(--gray5)',
-        6: 'var(--gray6',
+        6: 'var(--gray6)',
       },
 
       blue: 'var(--blue)',
-      darkblue: 'var(--dark-blue)',
+      darkblue: 'var(--darkblue)',
       red: 'var(--red)',
       green: 'var(--green)',
     },
 
-    extend: {},
+    borderRadius: {
+      md: '.625rem',
+      lg: '.75rem',
+      xl: '1.125rem',
+      '2xl': '1.5rem',
+      full: '9999px',
+    },
+
+    boxShadow: {
+      DEFAULT: '0px 10px 30px rgba(51, 51, 51, 0.1)',
+    },
+
+    extend: {
+      fontSize: {
+        xxs: '.625rem'
+      },
+    },
   },
   variants: {
     extend: {},
